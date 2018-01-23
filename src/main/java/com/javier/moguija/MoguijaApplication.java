9c45dc5b53ccc7717423275830c5d48f3b202724
gabriel <>
2018-01-22 22:40:18 -0300

    inicio aplicacion

---
+++ src/main/java/com/javier/moguija/MoguijaApplication.java
@@ -0,0 +1,12 @@
+package com.javier.moguija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoguijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoguijaApplication.class, args);
+	}
+}
